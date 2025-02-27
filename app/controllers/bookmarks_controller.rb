58280ba7b14d86373a69334456e3d1d860e30313
Tony Udotong <tonyudotong@gmail.com>
2025-02-27 14:40:30 +0000

    lists nd bookmarks

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+
+  before_action :set_list, only: %i[new create ]
+
+  def new
+    # We need @restaurant in our `simple_form_for`
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+end
